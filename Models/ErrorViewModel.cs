a9d038e6736fb0e9b46adfcbbb7544d02780eff5
John Doe <john.doe@contoso.com>
2025-04-22 22:16:42 +0200

    Init repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AZ400Learning.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
